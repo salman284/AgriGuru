0e6a0e4405d57cebc228c8fad4e73c729fd59840
shriom17 <129147531+shriom17@users.noreply.github.com>
2025-09-27 12:19:51 +0530

    fix

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -9,5 +9,25 @@ module.exports = function override(config, env) {
     loader: require.resolve('json-loader')
   });
 
+  // Force CI to be false
+  process.env.CI = 'false';
+  process.env.GENERATE_SOURCEMAP = 'false';
+  
+  // Disable ESLint plugin completely
+  if (config.plugins) {
+    config.plugins = config.plugins.filter(plugin => {
+      // Remove ESLint plugin entirely
+      if (plugin && plugin.constructor && plugin.constructor.name === 'ESLintWebpackPlugin') {
+        return false;
+      }
+      return true;
+    });
+  }
+
+  // Disable source maps in production
+  if (env === 'production') {
+    config.devtool = false;
+  }
+
   return config;
 }
